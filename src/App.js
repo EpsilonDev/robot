a98588af7b071697b4eac42c2c3be504a37e3052
epsilon <oura.dbk@gmail.com>
2020-01-04 10:56:44 +0100

    robot second v

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 let utils = [
   {
     nbr: 78,
-    nom: "Moujib",
+    nom: "Ouajib",
     filiere: "Isil"
   },
   {
@@ -23,6 +23,18 @@ let utils = [
   {
     nbr: 190,
     nom: "Sadik",
+    filiere: "Isil"
+  }
+  ,
+  {
+    nbr: 200,
+    nom: "Boudar",
+    filiere: "GE"
+  }
+  ,
+  {
+    nbr: 300,
+    nom: "Oudrar",
     filiere: "Isil"
   }
 ];
@@ -62,15 +74,17 @@ class App extends React.Component {
     <div class="row">
       <div class="col-sm-6 ">
         <ul class="list-group">
-          <li class="list-group-item"> { utils[0].nom.toLowerCase().includes(this.state.text, 0) ? <Carte nbr = {utils[0].nbr} nom = {utils[0].nom} filiere = {utils[0].filiere}></Carte> : null } </li>
-          <li class="list-group-item"> { utils[1].nom.toLowerCase().includes(this.state.text, 0)  ? <Carte nbr = {utils[1].nbr} nom = {utils[1].nom} filiere = {utils[1].filiere}></Carte> : null }  </li>
+          <li class="list-group-item"> { utils[0].nom.toLowerCase().includes(this.state.text.toLowerCase(), 0) ? <Carte nbr = {utils[0].nbr} nom = {utils[0].nom} filiere = {utils[0].filiere}></Carte> : null } </li>
+          <li class="list-group-item"> { utils[1].nom.toLowerCase().includes(this.state.text.toLowerCase(), 0)  ? <Carte nbr = {utils[1].nbr} nom = {utils[1].nom} filiere = {utils[1].filiere}></Carte> : null }  </li>
+          <li class="list-group-item"> { utils[4].nom.toLowerCase().includes(this.state.text.toLowerCase(), 0)  ? <Carte nbr = {utils[4].nbr} nom = {utils[4].nom} filiere = {utils[4].filiere}></Carte> : null }  </li>
         </ul>
 
       </div>
       <div class="col-sm-6">
       <ul class="list-group">
-          <li class="list-group-item"> { utils[2].nom.toLowerCase().includes(this.state.text, 0)  ? <Carte nbr = {utils[2].nbr} nom = {utils[2].nom} filiere = {utils[2].filiere}></Carte> : null } </li>
-          <li class="list-group-item"> { utils[3].nom.toLowerCase().includes(this.state.text, 0) ? <Carte nbr = {utils[3].nbr} nom = {utils[3].nom} filiere = {utils[3].filiere}></Carte> : null } </li>
+          <li class="list-group-item"> { utils[2].nom.toLowerCase().includes(this.state.text.toLowerCase(), 0)  ? <Carte nbr = {utils[2].nbr} nom = {utils[2].nom} filiere = {utils[2].filiere}></Carte> : null } </li>
+          <li class="list-group-item"> { utils[3].nom.toLowerCase().includes(this.state.text.toLowerCase(), 0) ? <Carte nbr = {utils[3].nbr} nom = {utils[3].nom} filiere = {utils[3].filiere}></Carte> : null } </li>
+          <li class="list-group-item"> { utils[5].nom.toLowerCase().includes(this.state.text.toLowerCase(), 0) ? <Carte nbr = {utils[5].nbr} nom = {utils[5].nom} filiere = {utils[5].filiere}></Carte> : null } </li>
         </ul>
       </div>
     </div>
